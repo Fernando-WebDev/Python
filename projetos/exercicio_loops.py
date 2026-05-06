eadafc9d438650fdaa365688685ed326918134c0
Fernando Henrique <fernandohenriquemoura10@gmail.com>
2026-05-06 11:23:34 -0300

    exercicios de repetições

---
+++ projetos/exercicio_loops.py
@@ -0,0 +1,40 @@
+x = 1
+while x < 6:
+    x += 2
+    print(x)
+print("...........")
+
+x = 9
+while x > 3:
+    print(x)
+    x -= 2
+print("...........")
+
+x = 9
+while x != 3:
+    print(x)
+    x -= 2
+print("...........")
+
+x = 4
+while x < 20:
+    print(x)
+    x += 3
+print("...........")
+
+x = 21
+while x > 11:
+    print(x)
+    x -= 2
+print("...........")
+
+x = -3
+while x < 12:
+    print(x)
+    x += 3
+print("...........")
+
+x = 2
+while x > -16:
+    print(x)
+    x -= 3
