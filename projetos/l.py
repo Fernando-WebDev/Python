26a6513384d8c601dbe343cef60565d94d42f62e
Fernando Henrique <fernandohenriquemoura10@gmail.com>
2026-05-21 09:00:36 -0300

    exercicios de for e while com lista

--- projetos/l.py
+++ projetos/l.py
@@ -1,6 +1,30 @@
+'''
 l = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
 print(l[5:16]) #fatiar a lista da posição 5 à 16
-print(l[2:18:3])
+print(l[2:18:3]) # l[inicio:fim:espaçamento]
 print(l[3:18:7])
 print(l[:17:2])
 print(l[5::4])
+
+print(l)
+l.sort() # o .sort() ordena em ordem númerica ou afalbetica
+print(l)
+'''
+
+n = []
+acu = 0
+qntdd = 0
+media = 0
+menor = 0
+maior = 0
+iguais = 0
+cont = True
+
+while cont == True:
+
+    n_ou_sair = input('Digite uma nota ou "s" para sair: ').lower
+
+    if 's' in n_ou_sair:
+        cont = False
+    else:
+        cont = True
