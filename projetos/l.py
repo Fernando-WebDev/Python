98024c31a19c31168e057c84c9afa28b9ba671ce
Fernando Henrique <fernandohenriquemoura10@gmail.com>
2026-05-21 08:24:32 -0300

    salvando exercicio de lista

---
+++ projetos/l.py
@@ -0,0 +1,6 @@
+l = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+print(l[5:16]) #fatiar a lista da posição 5 à 16
+print(l[2:18:3])
+print(l[3:18:7])
+print(l[:17:2])
+print(l[5::4])
