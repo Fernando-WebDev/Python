ab7cbc247f5e6cdb842d2b50390b4cb394ffca0f
Fernando Henrique <fernandohenriquemoura10@gmail.com>
2026-06-15 15:07:17 -0300

    comentando o código e finalizando o descodificador

--- projetos/codificando_palavras.py
+++ projetos/codificando_palavras.py
@@ -1,7 +1,7 @@
 def codificador():
 
     try:
-
+        #Coleta e definição de dados
         l = []
         l_letras_cod = []
         l_asc = []
@@ -10,24 +10,28 @@ def codificador():
         quant = int(input('Quantos nomes pretende digiar? '))
         key = int(input("Insira um chave de codificação: "))
 
+    #Exvessão para valores errados
     except  ValueError:
         print("Insira a quantidade em números inteiros!\n................................................")
         return codificador()
  
     else:
 
+        #Coleta de palavras a codificar
         rep = 0
         while rep < quant:
             nomes = input('Digite um nome: ')
             l.append(nomes)
             rep += 1
 
+        #Analisando palavra por palavra
         for itens in l:
             acomulado = ''
             acomulado_cod = ''
             codificacao = ''
-            for letras in itens:
 
+            #Descodificando letra por letra
+            for letras in itens:
                 asc = ord(letras)
                 print(f'CARACTERS: ', letras  , ', ASCII = ', asc)
                 if asc < 100:
@@ -46,10 +50,12 @@ def codificador():
                      acomulado_cod += str(asc)
                      codificacao += chr(asc)
 
+            #Adicionando as palavras codificadas a lista
             l_letras_cod.append(codificacao)
             l_asc.append(acomulado)
             l_asc_codificado.append(acomulado_cod)
         
+        #Mostrando os valores finais ao usuario
         print('Suas palavras: ', l)
         print('Suas palavras codificadas: ', l_letras_cod)
         print('ASCII: ', l_asc)
--- projetos/descodificador_palavras.py
+++ projetos/descodificador_palavras.py
@@ -1,27 +1,44 @@
 def descodificador():
     
     try:
+        #coleta e definição de dados
         palavras_cod = []
-        valores_asc = []
+        palavras_descod = []
+        valores_asc_descod = []
         palavras = ''
-        asc = ''
         key = int(input('Dgite a key: '))
         quantidade_palavras = int(input('Quantidade de palavras que deseja descodificar: '))
 
+    #excessão para valores errados
     except ValueError:
         print('Informações invalidas! favor digitar somente números.\n.................................................')
         return descodificador()
     
     else:
         
+        #Coleta de palavras códificadas
         rep = 0
         while rep < quantidade_palavras:
             palavras = input('Digite a palavra codificada: ')
-            asc = input('Digite o código ASCII codificado: ')
             rep += 1
             palavras_cod.append(palavras)
-            valores_asc.append(asc)
 
+        #Analise das palavras códificadas
         for itens in palavras_cod:
-            for letras in itens:
-                
+            palavra_descod = ''
+            asc_descod = ''
+
+            #Descodificando letra por letra
+            for letras_cod in itens:
+                asc_descod += str(ord(letras_cod) - key)
+                palavra_descod += chr(ord(letras_cod) - key)
+            
+            #Adicionando a palavra descodificada a lista
+            palavras_descod.append(palavra_descod)
+            valores_asc_descod.append(asc_descod)
+
+        #Mostrando os valores descodificados
+        print(palavras_descod)
+        print(valores_asc_descod)
+
+descodificador()
