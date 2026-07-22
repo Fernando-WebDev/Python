03231e0c13005607c140d8e657cece01ce7b1e38
Fernando Henrique <fernandohenriquemoura10@gmail.com>
2026-07-22 12:01:37 -0300

    novo codificador

---
+++ projetos/codificador_inverso.py
@@ -0,0 +1,49 @@
+def codificador_inverso():
+
+    try:
+        para_descod = []
+        cod = []
+        palavra = ''
+        quant = int(input('Quantas palavras/frases vai digitar? '))
+
+    except ValueError:
+
+        print('Informe um valor valido!.........................')
+        return codificador_inverso()
+    else:
+
+        x = 0
+        while x < quant:
+            palavra = input('Digite uma palavra/frase: ')
+            para_descod.append(palavra)
+            x += 1
+
+        for palavras in para_descod:
+            palavar_cod = ''
+            for letra in palavras:
+                nova_letra = ''
+
+                if 'A'<= letra <= 'Z':
+                    nova_letra = ord(letra) - 65
+                    nova_letra = chr(90 - nova_letra)
+
+                elif 'a' <= letra <= 'z':
+                    nova_letra = ord(letra) - 97
+                    nova_letra = chr(122 - nova_letra)
+
+                else: 
+                    nova_letra = letra
+
+                palavar_cod += nova_letra
+
+        cod.append(palavar_cod)
+
+        crip_inversa = open('/home/fer/Documentos/GitHub/Python/crip_inversa.txt', 'w')
+        adicionada = ''
+        for item in cod:
+            adicionada = item
+            crip_inversa.write(adicionada)
+
+        crip_inversa.close()
+
+codificador_inverso()
