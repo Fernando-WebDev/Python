cb68bddd5950fd4f90cd1bc4e8d45ed68c2145ec
Fernando Henrique <fernandohenriquemoura10@gmail.com>
2026-07-27 16:39:21 -0300

    deletei a parte desnecessaria do descodificador

--- projetos/descodificador_geral.py
+++ projetos/descodificador_geral.py
@@ -71,50 +71,19 @@ def descodificador():
         #Fechando o arquivo
         criptografia.close()
     
-    try:
-
-        escolher_metodo = int(input('Metodos:\n1-Descodicifar ASCII\n2-Descodiicar altbash simples\nOBS: Insira somente 1 ou 2!\nResposta: '))
-        
-        if escolher_metodo == 1:
-            descodificar_ASCII()
-            exit()
-        elif escolher_metodo == 2:
-            descodificar_altbash()
-            exit()
-        else:
-            print('Informação invalida! Tente novamente.\n.........................................')
-            return descodificador()
-
-    #excessão para valores errados
-    except ValueError:
-        print('Informações invalidas! favor digitar somente números.\n.................................................')
-        return descodificador()
     
+
+    escolher_metodo = int(input('Metodos:\n1-Descodicifar ASCII\n2-Descodiicar altbash simples\nOBS: Insira somente 1 ou 2!\nResposta: '))
+    
+    if escolher_metodo == 1:
+        descodificar_ASCII()
+        exit()
+    elif escolher_metodo == 2:
+        descodificar_altbash()
+        exit()
     else:
-        
-        #Coleta de palavras códificadas
-        rep = 0
-        while rep < quantidade_palavras:
-            palavras = input('Digite a palavra codificada: ')
-            rep += 1
-            palavras_cod.append(palavras)
+        print('Informação invalida! Tente novamente.\n.........................................')
+        return descodificador()
 
-        #Analise das palavras códificadas
-        for itens in palavras_cod:
-            palavra_descod = ''
-            asc_descod = ''
-
-            #Descodificando letra por letra
-            for letras_cod in itens:
-                asc_descod += str(ord(letras_cod) - key)
-                palavra_descod += chr(ord(letras_cod) - key)
-            
-            #Adicionando a palavra descodificada a lista
-            palavras_descod.append(palavra_descod)
-            valores_asc_descod.append(asc_descod)
-
-        #Mostrando os valores descodificados
-        print(palavras_descod)
-        print(valores_asc_descod)
-
+    
 descodificador()
